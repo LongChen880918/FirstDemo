efe713a940aa0db3d75548b26e2c23e07c257c33
LongChen880918 <657967440@qq.com>
2019-05-29 09:11:39 +0800

    java8新特性，集合里的数据进行过滤去重

---
+++ src/main/java/com/atguigu/gmall/service/imp/FunctionX.java
@@ -0,0 +1,69 @@
+package com.atguigu.gmall.service.imp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+public class FunctionX {
+	public static void main(String[] args) {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "a");
+		map.put("price",500);
+		map.put("SFZH",360426);
+
+		Map<String, Object> map2 = new HashMap<String, Object>();
+		map2.put("name", "a");
+		map2.put("price",1500);
+		map2.put("SFZH",360426);
+
+		Map<String, Object> map3 = new HashMap<String, Object>();
+		map3.put("name", "b");
+		map3.put("price",300);
+		map3.put("SFZH",360000);
+
+		List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
+		list.add(map);
+		list.add(map2);
+		list.add(map3);
+
+
+
+		List<Map<String,Object>> result =new ArrayList<>();
+
+		Map<String, List<Map<String, Object>>> glist = list.stream().collect(Collectors.groupingBy(e -> e.get("SFZH").toString()));
+		
+		list.stream().collect(Collectors.groupingBy(e->e.get("SFZH").toString()));
+		
+		
+		glist.forEach((k,slist)->{
+		Map<String,Object> nmap=new HashMap<>();
+		IntSummaryStatistics sumcc = slist.stream().collect(Collectors.summarizingInt(e->Integer.valueOf("1")));
+		
+		IntSummaryStatistics intSummaryStatistics = slist.stream().collect(Collectors.summarizingInt(e->Integer.valueOf("1")));
+		
+		
+		if(sumcc.getSum()>1) {
+			slist.forEach(x->{
+				result.add(x);
+			});
+			
+		}
+		System.out.println("sumcc:"+sumcc.toString());
+		System.out.println("k："+k);
+		System.out.println("slist:"+slist); 
+		
+		}); 
+		
+		System.out.println(glist.toString());
+		
+		System.out.println("--------testMerge-------------");
+		result.forEach(x->{
+			System.out.println(x);
+		});
+	}
+}
---
+++ src/main/java/com/atguigu/gmall/service/imp/FunctionY.java
@@ -0,0 +1,50 @@
+package com.atguigu.gmall.service.imp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FunctionY {
+	public static void main(String[] args) {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("name", "a");
+		map.put("price",500);
+		map.put("SFZH",360426);
+
+		Map<String, Object> map2 = new HashMap<String, Object>();
+		map2.put("name", "a");
+		map2.put("price",1500);
+		map2.put("SFZH",360426);
+
+		Map<String, Object> map3 = new HashMap<String, Object>();
+		map3.put("name", "b");
+		map3.put("price",300);
+		map3.put("SFZH",360000);
+
+		List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
+		list.add(map);
+		list.add(map2);
+		list.add(map3);
+		List ids = new ArrayList();
+		List count = new ArrayList();
+		List<Map<String, Object>> personList = list.stream().filter(// 过滤去重
+           v -> { 
+        	   String name = v.get("name").toString()+v.get("price").toString();
+        	   int num =1;
+               boolean flag = !ids.contains(name);
+               if(!flag) {
+            	   count.add(num);
+               }
+               ids.add(name);
+               return flag;
+           }
+	       ).collect(Collectors.toList()); 
+       System.out.println(personList);
+       System.out.println(ids);
+       System.out.println(count);
+	}
+
+}
